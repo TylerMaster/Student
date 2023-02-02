3b849ae5d120699d0d52b92caf3533917410b546
Brandon Smith <brandondmt3@gmail.com>
2023-02-02 11:47:28 -0500

    put back to original size

--- src/student/StudentList.java
+++ src/student/StudentList.java
@@ -12,7 +12,7 @@ public class StudentList {
     
     public static void main(String[] args){
         //Student s1 = new Student("s1", "ronak");
-        Student[] StudentList = new Student[6];
+        Student[] StudentList = new Student[3];
         StudentList[0]=new Student("s1", "ronak");
         StudentList[1]=new Student("s2", "abc");
         StudentList[2]=new Student("s3", "xyz");
